6ea567f0d9361d1ba2742ec7cd835f6adb23307f
Cihad Paksoy <cihadp@gmail.com>
2023-04-25 11:32:41 +0300

    remove unnecessary base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rejenerasyon.com.tr',
-  base: '/rejenerasyon',
+  site: 'https://www.rejenerasyon.com.tr',
   experimental: {
     assets: true
   }
